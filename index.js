b8631c21fb488705bbbafd51a0e073bb87cb0eec
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-09-02 00:33:24 -0300

    dia 1 - libs feitas mas primeiro post nao funcionando

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 import express, { application } from 'express';
+import joi from 'joi';
 import cors from 'cors';
 import {MongoClient, ObjectId} from 'mongodb';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+dotenv.config();
 
 let app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
-app.listen(5000, ()=>{
+mongoClient.connect(()=>{
+    db = mongoClient.db('database');
+});
+
+app.post('/participants', async(res,req)=>{
+    let name = req.body;
+    console.log(name);
+    const userSchema = joi.object({
+    name: joi.string().min(0)
+});
+
+const validation = userSchema.validate(name, {abortEarly: true});
+
+if(validation.error){
+return res.status(422).send(validation.error.details)        
+}
+const objUser={
+    name,
+    lastStatus: Date.now()
+}
+
+try{
+await db.collection('participants').insertOne(objUser);
+console.log("foi")
+}catch{
+    console.log("deu ruim")
+}
+
+})
+
+
+
+app.listen(5001, ()=>{
     console.log("listening port 5000");
 })
