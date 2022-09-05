83653262b268135ab3d563d70ad58a7c3942de7e
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-09-04 22:18:33 -0300

    post messages feito

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const objUser={
     lastStatus: Date.now()
 }
 
-const day = dayjs().format('HH:MM:SS')
+const day = dayjs().format('HH:MM:SS');
 
 const objLogin={
     from: name,
@@ -75,35 +75,51 @@ res.sendStatus(500);
 app.post('/messages', async(req,res)=>{
     const {to,text,type} = req.body;
     const {user} = req.headers;
-    let objMes;
-
-    const person = participants.find(usr=> usr===user);
+    let objMes = {};
+    const person = participants.find(usr=> usr.name===user);
+    console.log(person);
+    const day = dayjs().format('HH:MM:SS');
     if(person){  
         objMes = {
         to,
         text,
         type,
-        from:person
-    }}
+        from:person.name,
+        time: day
+    }}else{
+       return res.sendStatus(422);
+    }
   
 const userSchema = joi.object({
-    to: joi.string().min(0),
-    text: joi.string().min(0),
-    type: joi.valid('message','private_message'),
+    to: joi.string().min(1).required(),
+    text: joi.string().min(1).required(),
+    type: joi.valid('message','private_message').required(),
+    from: joi.required(),
+    time: joi.required()
 });
 
 const validation = userSchema.validate(objMes);
 if(validation.error){
-    return res.status(422).send(validation.error.details)        
+    res.status(422).send(validation.error.details)        
     }else{
-        res.send(objMes)
+        const message = db.collection('messages').insertOne(objMes);
+        res.send(201);
     }
+})
 
-
+app.get('/messages', async(req, res)=>{
+    try{
+const messages = await db.collection('messages').find().toArray();
+res.send(messages)
+    }catch{
+res.send("deu reuim");
+    }
 })
 
 
-app.listen(5001, ()=>{
+
+
+app.listen(5000, ()=>{
     console.log("listening port 5000");
 })
 
